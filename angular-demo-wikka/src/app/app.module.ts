630597248a05fd7bfc7d3f8f0ac4c321984b5372
V+ <wikka9088@gmail.com>
2018-03-12 23:29:11 -0400

    ng generate component houses

--- angular-demo-wikka/src/app/app.module.ts
+++ angular-demo-wikka/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HousesComponent } from './houses/houses.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HousesComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-demo-wikka/src/app/houses/houses.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-houses',
+  templateUrl: './houses.component.html',
+  styleUrls: ['./houses.component.css']
+})
+export class HousesComponent implements OnInit {
+
+  house = '616 Egret Way';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
