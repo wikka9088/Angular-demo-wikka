ea2a81fe266f6774d958e28e105538c10d40e55c
V+ <wikka9088@gmail.com>
2018-03-19 23:06:18 -0400

    create the house property in house-detail.ts, remember useing @Input() to decorate because the external HousesComponent will bind to it by [house]="selectedHouse"

---
+++ angular-demo-wikka/src/app/house-detail/house-detail.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {House} from "../house";
+
+@Component({
+  selector: 'app-house-detail',
+  templateUrl: './house-detail.component.html',
+  styleUrls: ['./house-detail.component.css']
+})
+export class HouseDetailComponent implements OnInit {
+
+  @Input() house : House;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
