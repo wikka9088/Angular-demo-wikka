64f72f81c346b23e0060354b70a790ffcca786e4
V+ <wikka9088@gmail.com>
2018-03-25 22:59:46 -0400

    display messages in message component

--- angular-demo-wikka/src/app/messages/messages.component.ts
+++ angular-demo-wikka/src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MessagesService} from "../messages.service";
 
 @Component({
   selector: 'app-messages',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  //The messageService property must be public because you're about to bind to it in the template.
+  constructor(public messagesService: MessagesService) { }
 
   ngOnInit() {
   }
