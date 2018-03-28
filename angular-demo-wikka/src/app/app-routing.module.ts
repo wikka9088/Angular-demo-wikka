7b77fea96e3ead4f50e279710eb2c07b9c9d9f99
V+ <wikka9088@gmail.com>
2018-03-27 23:45:21 -0400

    Add an @NgModule.exports array with RouterModule in it. Exporting RouterModule makes router directives available for use in the AppModule components that will need them.

--- angular-demo-wikka/src/app/app-routing.module.ts
+++ angular-demo-wikka/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
     CommonModule
   ],
+
+  exports: [
+    RouterModule
+  ],
+
   declarations: []
 })
 export class AppRoutingModule { }
