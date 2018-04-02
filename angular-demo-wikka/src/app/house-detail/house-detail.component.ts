76cfc91fb0d739c0e184ddc6a50a6ac7b0ba9230
V+ <wikka9088@gmail.com>
2018-04-01 23:18:57 -0400

    routeable HouseDetailComponent, link it to the HouseService, and add go back button, use this.location.back()

--- angular-demo-wikka/src/app/house-detail/house-detail.component.ts
+++ angular-demo-wikka/src/app/house-detail/house-detail.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {House} from "../house";
-import {HOUSES} from "../../mock-data/mock-houses";
+import {HouseService} from "../house.service";
+import {ActivatedRoute} from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-house-detail',
@@ -12,13 +14,25 @@ export class HouseDetailComponent implements OnInit {
   @Input() house : House;
 
 
-  constructor() { }
+  constructor(
+      private houseService: HouseService,
+      private route: ActivatedRoute,
+      private location: Location
+  ) { }
 
   ngOnInit() {
+      this.getHouse();
   }
 
-  getHouses() : House[] {
-      return HOUSES;
+  getHouse(): void
+  {
+      //The route.snapshot is a static image of the route information shortly after the component was created.
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.houseService.getHouse(id).subscribe(house => this.house = house)
   }
+
+    goBack(): void {
+        this.location.back();
+    }
 
 }
--- angular-demo-wikka/src/app/house.service.ts
+++ angular-demo-wikka/src/app/house.service.ts
@@ -11,6 +11,11 @@ export class HouseService {
   //Angular will inject the singleton MessagesService into that property when it creates the HouseService.
   constructor(private messageService: MessagesService) { }
 
+  getHouse(id: number): Observable<House> {
+    this.messageService.addMessage(`HeroService: fetched hero id=${id}`);
+    return of(HOUSES.find(house => house.id === id));
+  }
+
   getHouses(): Observable<House[]> {
     //send the message _after_ fetching the houses
     this.messageService.addMessage('HouseService: Fetched the houses');
