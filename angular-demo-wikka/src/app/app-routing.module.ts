0c666c0372df8afbe9bbc0cb6736c7b5ab543433
V+ <wikka9088@gmail.com>
2018-04-01 21:51:09 -0400

    delete house details from HousesComponent, add a router for detail, add routerLink in dashboard for details

--- angular-demo-wikka/src/app/app-routing.module.ts
+++ angular-demo-wikka/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HousesComponent} from "./houses/houses.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HouseDetailComponent} from "./house-detail/house-detail.component";
 
 
 const routes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: 'houses', component: HousesComponent},
     {path: 'dashboard', component: DashboardComponent},
+    {path: 'detail/:id', component: HouseDetailComponent}
 ];
 
 @NgModule({
