916105262707bbeb966cb94993544b6d3b70cc53
V+ <wikka9088@gmail.com>
2018-04-01 21:34:02 -0400

    add dashboard route in app-routing.module.ts

--- angular-demo-wikka/src/app/app-routing.module.ts
+++ angular-demo-wikka/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HousesComponent} from "./houses/houses.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 
 const routes: Routes = [
-    {path: 'houses', component: HousesComponent}
+    {path: 'houses', component: HousesComponent},
+    {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
